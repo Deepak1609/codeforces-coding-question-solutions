6aaa5da841dc0212e064df8f37a4fd92aca25e70
Deepak <54376136+Deepak1609@users.noreply.github.com>
2024-06-07 11:34:39 +0530

    E. Area of a Circle
    E. Area of a Circle
    time limit per test1 second
    memory limit per test256 megabytes
    inputstandard input
    outputstandard output
    Given a number R calculate the area of a circle using the following formula:
    
    Area = π * R2.
    
    Note: consider π = 3.141592653.
    
    Input
    Only one line containing the number R (1  ≤  R  ≤  100).
    
    Output
    Print the calculated area, with 9 digits after the decimal point.

---
+++ contest-sheet-1-data-type-conditions/Area of a Circle.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+
+using namespace std;
+const double pi = 3.141592653;
+int main()
+{
+    double d;
+    cin>>d;
+    cout<<fixed<<setprecision(9)<<pi * d * d<<endl;
+
+    return 0;
+}
+
+
