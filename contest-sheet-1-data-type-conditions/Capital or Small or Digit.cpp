76b2314378bb0fc201f90b11e50fb87aa38ece08
Deepak <54376136+Deepak1609@users.noreply.github.com>
2024-06-07 12:04:14 +0530

    M. Capital or Small or Digit
    M. Capital or Small or Digit
    time limit per test1 second
    memory limit per test256 megabytes
    inputstandard input
    outputstandard output
    Given a letter X. Determine whether X is Digit or Alphabet and if it is Alphabet determine if it is Capital Case or Small Case.
    
    Note:
    
    Digits in ASCII '0' = 48,'1' = 49 ....etc
    Capital letters in ASCII 'A' = 65, 'B' = 66 ....etc
    Small letters in ASCII 'a' = 97,'b' = 98 ....etc
    Input
    Only one line containing a character X which will be a capital or small letter or digit.
    
    Output
    Print a single line contains "IS DIGIT" if X is digit otherwise, print "ALPHA" in the first line followed by a new line that contains "IS CAPITAL" if X is a capital letter and "IS SMALL" if X is a small letter

---
+++ contest-sheet-1-data-type-conditions/Capital or Small or Digit.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c;
+    cin>>c;
+    if(c>= 48 && c<=57)
+    {
+        cout<<"IS DIGIT"<<endl;
+    }
+    else if(c>=65 && c<=90)
+    {
+        cout<<"ALPHA"<<endl<<"IS CAPITAL"<<endl;
+    }
+    else
+    {
+        cout<<"ALPHA"<<endl<<"IS SMALL"<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
