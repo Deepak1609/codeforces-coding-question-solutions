dcd33fb2cf1b3996d7731d21649a57b995c3ccf6
Deepak <54376136+Deepak1609@users.noreply.github.com>
2024-06-07 12:46:02 +0530

    B. Memo and Momo
    B. Memo and Momo
    time limit per test1 second
    memory limit per test256 megabytes
    inputstandard input
    outputstandard output
    Memo and Momo are playing a game. Memo will choose a positive number a
    , and Momo will choose a positive number b
    .
    
    Your task is to tell them who will win according to the following rules:
    
    If both a
     and b
     are divisible by k
    , both of them win and you should print "Both".
    If a
     is divisible by k
     but b
     isn't, Memo wins and you should print "Memo".
    If b
     is divisible by k
     but a
     isn't, Momo wins and you should print "Momo".
    If both a
     and b
     are not divisible by k
    , no one wins and you should print "No One".
    Input
    Only one line containing three positive numbers a
    , b
     and k
     (1≤a,b,k≤1018
    ).
    
    Output
    Print the answer as described in the statement.

---
+++ contestsheet-1/Memo and Momo.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    long long memo, momo, k;
+    cin>>memo>>momo>>k;
+
+    if(memo % k == 0 && momo % k == 0)
+    {
+        cout<<"Both"<<endl;
+    }
+    else if (memo % k == 0 && momo % k != 0)
+    {
+        cout<<"Memo"<<endl;
+    }
+    else if(memo % k != 0 && momo % k == 0)
+    {
+        cout<<"Momo"<<endl;
+    }
+    else
+    {
+        cout<<"No One"<<endl;
+    }
+
+
+
+
+    return 0;
+}
