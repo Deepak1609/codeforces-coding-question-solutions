b16c1b5b14c49cd1023cc8d8e5d2028987f8d363
Deepak <54376136+Deepak1609@users.noreply.github.com>
2024-06-22 00:39:23 +0530

    Y. Easy Fibonacci
    Y. Easy Fibonacci
    time limit per test1 second
    memory limit per test256 megabytes
    input standard input
    outputstandard output
    Given a number N. Print first N numbers of the Fibonacci sequence.
    
    Note: In order to create the Fibonacci sequence use the following function:
    
    fib(1) = 0.
    fib(2) = 1.
    fib(n) = fib(n - 1) + fib(n - 2).
    Input
    Only one line containing a number N (1 ≤ N ≤ 45).
    
    Output
    Print the first N numbers from the Fibonacci Sequence .

---
+++ sheet-2-loops/Easy Fibonacci.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a = 0, b = 1, sum = 0;
+
+    //for(int i = 1; i<=n; i++)
+    //{
+        if(n == 1)
+        {
+            cout<<a<<endl;
+        }
+        if(n == 2)
+        {
+            cout<<a<<" "<<b<<endl;
+        }
+        else
+        {
+            cout<<a<<" "<<b<<" ";
+            while(n - 2>0)
+            {
+                sum = a + b;
+                a = b;
+                b = sum;
+                cout<<sum<<" ";
+                n--;
+            }
+
+
+        }
+   // }
+    return 0;
+}
