5fd98e27e10a8f3194e5658ced4513513cc83f16
Deepak <54376136+Deepak1609@users.noreply.github.com>
2024-06-25 15:37:18 +0530

    D. Range Sum
    D. Range Sum
    time limit per test1 second
    memory limit per test256 megabytes
    input standard input
    outputstandard output
    You are given a range represented by two integers L
     and R
    , and you should find the sum of the numbers in the range between L
     and R
     inclusive.
    
    Input
    First line contains a number T
     (1≤T≤105
    ) – the number of test cases.
    
    Each of the next T
     lines contains two numbers L,R
     (1≤L,R≤109
    ).
    
    Output
    For each test case, print the sum.

--- contest-sheet-2/Range Sum.cpp
+++ contest-sheet-2/Range Sum.cpp
@@ -2,18 +2,24 @@
 using namespace std;
 int main()
 {
-    long long t,l,r,sum;
+    long long t,l,r,maxi,mini;
     cin>>t;
 
     while(t--)
     {
         cin>>l>>r;
-        sum = 0;
+        /*sum = 0;
         for(int i = l; i<=r; i++)
         {
             sum += i;
         }
-        cout<<sum<<endl;
+        cout<<sum<<endl;*/
+        maxi = max(l,r);
+        mini = min(l,r);
+        mini--;
+        long long r1 = mini * (mini + 1) / 2;
+        long long r2 = maxi * (maxi + 1) / 2;
+        cout<<r2-r1<<endl;
     }
     return 0;
 }
